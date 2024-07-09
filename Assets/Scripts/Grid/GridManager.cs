1879adf248c9906be1fbe4bd5e42027da38ee986
Emir0zcelik <themirozcelik@gmail.com>
2024-07-10 01:42:56 +0300

    Borders of grid
    Implemented sphagetti code. Will be more clean in future

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -9,8 +9,11 @@ public class GridManager : Singleton<GridManager>
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private int cellSize;
+    [SerializeField] private int borderSize = 5;
     [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GameObject edgeTreePrefab;
     List<GameObject> instatiatedTiles;
+    [SerializeField] private List<Material> edgeMaterialList;
 
     public Grid<Tile> _grid;
     private void Awake() {
@@ -31,10 +34,49 @@ public class GridManager : Singleton<GridManager>
                 GameObject tempPrefab = Instantiate(
                     tilePrefab, 
                     position,
-                    Quaternion.identity);
-                    
+                    Quaternion.identity
+                    );
+
                 instatiatedTiles.Add(tempPrefab);
                 tempPrefab.transform.SetParent(transform, false);
+
+                if (x < borderSize || x > width - borderSize - 1 || z < borderSize || z > height - borderSize -1)
+                {
+                    tempPrefab.GetComponent<MeshRenderer>().material = edgeMaterialList[0];
+                    GameObject edgeTreeTemp = Instantiate(
+                        edgeTreePrefab,
+                        Vector3.zero,
+                        Quaternion.identity
+                    );
+                    edgeTreeTemp.transform.SetParent(tempPrefab.transform, false);
+                    edgeTreeTemp.transform.localScale = new Vector3(0.1f, 1f, 0.1f);
+                    edgeTreeTemp.transform.position = new Vector3(tempPrefab.transform.position.x, 0, tempPrefab.transform.position.z - 1.25f);
+                }
+
+                int innerBorderSize = borderSize / 2;
+
+                if (x >= borderSize && x < borderSize + innerBorderSize && z >= borderSize && z <= height - borderSize -1)
+                {
+                    tempPrefab.GetComponent<MeshRenderer>().material = edgeMaterialList[1];
+                }
+
+                if (x >= width - (borderSize + innerBorderSize) && x <= width - borderSize - 1 && z >= borderSize && z <= height - borderSize -1)
+                {
+                    tempPrefab.GetComponent<MeshRenderer>().material = edgeMaterialList[1];
+                }
+
+                if (z >= borderSize && z < borderSize + innerBorderSize && x >= borderSize && x <= width - borderSize -1)
+                {
+                    tempPrefab.GetComponent<MeshRenderer>().material = edgeMaterialList[1];
+                }
+
+                if (z >= height - (borderSize + innerBorderSize) && z <= height - borderSize - 1 && x >= borderSize && x <= width - borderSize -1)
+                {
+                    tempPrefab.GetComponent<MeshRenderer>().material = edgeMaterialList[1];
+                }
+
+
+
                 
             }
         }
