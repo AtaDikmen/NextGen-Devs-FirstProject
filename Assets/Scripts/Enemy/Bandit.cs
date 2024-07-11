e1441bfbbb65250d21ffc2e89cdc9068a228d4e8
Hatice Esra Yılmaz <hesra.yilmazz@gmail.com>
2024-07-11 16:17:07 +0300

    arrow and bow directions fixed

--- Assets/Scripts/Enemy/Bandit.cs
+++ Assets/Scripts/Enemy/Bandit.cs
@@ -20,6 +20,7 @@ public class Bandit : Enemy
         enemyAI.Stop();
         transform.LookAt(target);
         animator.SetTrigger("Attack");
-        base.Attack();
+        base.Attack();       
     }
+
 }
