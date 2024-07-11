2923196957827f43d63aa57b5bbdb5adc954d8fb
Ata Dikmen <ata.dikmen35@gmail.com>
2024-07-11 19:47:36 +0300

    Emir merge

---
+++ Assets/Scripts/Resource Manager/Tree.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour, IDamagable
+{
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currentHealth;
+    [SerializeField] private float inactiveDuration = 5f;
+
+    MeshRenderer meshRenderer;
+    BoxCollider boxCollider;
+    private int woodRange;
+    private void Awake() {
+        woodRange = ResourceManager.Instance.woodRange;
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+        boxCollider = GetComponent<BoxCollider>();
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Tree health: " + currentHealth);
+        if (currentHealth <= 0)
+        {
+            OnDead();
+        }
+    }
+
+    public virtual void OnDead()
+    {
+        currentHealth = 0;
+        ResourceManager.Instance.AddWood(Random.Range(woodRange / 2, woodRange));
+        StartCoroutine(SetInactiveTemp());
+    }
+
+    private IEnumerator SetInactiveTemp()
+    {
+        meshRenderer.enabled = false;
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(inactiveDuration);
+        meshRenderer.enabled = true;
+        boxCollider.enabled = true;
+        currentHealth = maxHealth;
+    }
+}
